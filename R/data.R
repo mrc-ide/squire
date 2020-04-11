520c3e4cf2d58dd89e5f6fdf13259328ef802ef2
CharlieWhittaker <cwhittaker1000@gmail.com>
2020-04-11 13:18:45 +0100

    documenting healthcare and income group data

--- R/data.R
+++ R/data.R
@@ -13,3 +13,28 @@
 #' @format A data.frame
 # #' @source \url{http://www.diamondse.info/}
 "population"
+
+#' Country specific healthcare capacity
+#'
+#' Hospital and ICU beds per 1000 pop for various countries
+#'
+#' @format A data.frame
+# #' @source \url{http://www.diamondse.info/}
+"country_specific_healthcare_capacity"
+
+#' Income strata healthcare capacity
+#'
+#' Average Hospital and ICU beds per 1000 pop for different
+#' World Bank income groups
+#'
+#' @format A data.frame
+# #' @source \url{http://www.diamondse.info/}
+"country_specific_healthcare_capacity"
+
+#' Income group
+#'
+#' World Bank income group for all countries
+#'
+#' @format A data.frame
+# #' @source \url{http://www.diamondse.info/}
+"income_group"
