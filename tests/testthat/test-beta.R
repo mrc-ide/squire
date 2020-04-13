1061ea9ac9ae090ea59678d8e701f09261506acb
OJWatson <oj.watson@hotmail.co.uk>
2020-04-13 11:23:01 +0100

    Change dur_R and dur_hosp to be dur_IMild and dur_ICase to be clearer these are the durations of tehse states rather than the duration before moving into these states

--- tests/testthat/test-beta.R
+++ tests/testthat/test-beta.R
@@ -24,27 +24,27 @@ test_that("beta_explicit input check work", {
   mm_na <- mm
   mm_na[1] <- NA
 
-  expect_error(beta_est_explicit(dur_R = 0, dur_hosp = 5,
+  expect_error(beta_est_explicit(dur_IMild = 0, dur_ICase = 5,
                                  prob_hosp = c(0.2,0.1),
                                  mixing_matrix = mm, R0 = 2),
-               "dur_R must be greater than zero")
+               "dur_IMild must be greater than zero")
 
-  expect_error(beta_est_explicit(dur_R = 2, dur_hosp = 0,
+  expect_error(beta_est_explicit(dur_IMild = 2, dur_ICase = 0,
                                  prob_hosp = c(0.2,0.1),
                                  mixing_matrix = mm, R0 = 2),
-               "dur_hosp must be greater than zero")
+               "dur_ICase must be greater than zero")
 
-  expect_error(beta_est_explicit(dur_R = c(2,2), dur_hosp = 5,
+  expect_error(beta_est_explicit(dur_IMild = c(2,2), dur_ICase = 5,
                                  prob_hosp = c(0.2,0.1),
                                  mixing_matrix = mm, R0 = 2),
-               "dur_R must be of length 1")
+               "dur_IMild must be of length 1")
 
-  expect_error(beta_est_explicit(dur_R = 2, dur_hosp = 5,
+  expect_error(beta_est_explicit(dur_IMild = 2, dur_ICase = 5,
                                  prob_hosp = c(0.2,NA),
                                  mixing_matrix = mm, R0 = 2),
                "prob_hosp must not contain NAs")
 
-  expect_error(beta_est_explicit(dur_R = 2, dur_hosp = 5,
+  expect_error(beta_est_explicit(dur_IMild = 2, dur_ICase = 5,
                                  prob_hosp = c(0.2,0.2),
                                  mixing_matrix = mm_na, R0 = 2),
                "mixing_matrix must not contain NAs")
