673a95a3825942a1f417cc59547dfbc8881b58cc
CharlieWhittaker <cwhittaker1000@gmail.com>
2020-04-14 14:53:09 +0100

    capacity to provide summary metrics from format_output (age_disagg still to do)

--- R/format_output.R
+++ R/format_output.R
@@ -63,96 +63,117 @@ collapse_for_report <- function(d){
 #' @export
 format_output <- function(x, var_select = NULL, reduce_age = TRUE,
                           combine_compartments = TRUE, date_0 = NULL){
-  # Single Compartment Variables
-  single_variables <- c("S", "IMild", "R", "D", "n_E2_I",
-                        "n_E2_ICase1", "n_E2_IMild", "delta_D")
 
-  # Select variables
-  vars <- x$output
-
-  # Variable names
-  all_names <- names(vars[1,,1])
+  # Get relevant model details
+  nt <- nrow(x$output)
+  index <- odin_index(x$model)
+  all_names <- names(x$output[1,,1])
   all_names_simp <- gsub("\\[.*?]", "", all_names)
 
-  if(!all(var_select %in% c(all_names_simp, unique(gsub("[1-2]$", "", all_names_simp))))){
+  # Multi/Single Compartment Variables
+  single_compartments <- c("S", "IMild", "R", "D", "n_E2_I", "n_E2_ICase1", "n_E2_IMild", "delta_D")
+  multi_compartments <- c("E", "ICase", "IOxGetLive", "IOxGetDie", "IOxNotGetLive", "IOxNotGetDie",
+                          "IMVGetLive", "IMVGetDie", "IMVNotGetLive", "IMVNotGetDie", "IRec")
+
+  # Summary Values and Relevant Compartments
+  summary_variables <- c("deaths", "infections", "hospital_occupancy", "ICU_occupancy")
+  summary_variable_compartments <- list(deaths = "delta_D",
+                                        infections = "n_E2_I",
+                                        hospital_occupancy = c("IOxGetLive1","IOxGetLive2","IOxGetDie1","IOxGetDie2", "IRec1", "IRec2"),
+                                        ICU_occupancy = c("IMVGetLive1","IMVGetLive2","IMVGetDie1","IMVGetDie2"))
+
+  # Check var_select contains only variables described above
+  if(sum(!(var_select %in% c(single_compartments, multi_compartments, summary_variables))) > 0) {
     stop("Selected variable are not all present in output")
   }
 
-  # Select relevant column names for subsetting
-  #   if - if no variables are selected, use all for subsetting
-  #   else if - if some variables are selected and none are single-compartment variables,
-  #             extract all compartments with compartment_name1 or compartment_name2
-  #   else - if some of the variables are single-compartment variables, work through
-  #          each individually, treating single and double compartment variables differently
-  number_variables <- length(var_select)
-  if(is.null(var_select)){
-    var_select <- unique(all_names_simp[!all_names_simp %in% c("step", "time")])
-  } else if (!is.null(var_select) & sum(var_select %in% single_variables) == 0)  {
-    new_var_select <- c()
+  # Disaggregating var_select into compartments vs summary variables
+  compartments <- var_select[!(var_select %in% summary_variables)]
+  compartments <- if (identical(compartments, character(0))) NULL else compartments
+  summaries <- var_select[var_select %in% summary_variables]
+  summaries <- if (identical(summaries, character(0))) NULL else summaries
+
+  # Extracting relevant columns for compartment variables
+  # -> if var_select = NULL extract all compartments
+  # -> if var_select = names specific compartments, extract those
+  # -> if var_select = summary variables but no specific compartments, return empty list
+  if(is.null(var_select)) {
+    compartments <- unique(all_names_simp[!all_names_simp %in% c("step", "time")])
+    compartment_output_list <- lapply(compartments, function(j) {
+      odin_sv(x$output[,unlist(index[j]),], replicates = x$parameters$replicates, nt = nt)
+    })
+    names(compartment_output_list) <- compartments
+  } else if (!is.null(var_select) & !is.null(compartments)) {
+    number_variables <- length(compartments)
+    new_compartments <- c()
     for (i in 1:number_variables) {
-      temp <- unique(all_names_simp[grepl(paste0("^", var_select[i], "[1-2]"), all_names_simp)])
-      new_var_select <- c(new_var_select, temp)
-    }
-    var_select <- new_var_select
-  } else {
-    new_var_select <- c()
-    for (i in 1:number_variables) {
-      if (var_select[i] %in% single_variables) {
-        new_var_select <- c(new_var_select, var_select[i])
+      if (compartments[i] %in% single_compartments) {
+        new_compartments <- c(new_compartments, compartments[i])
       } else {
-        temp <- unique(all_names_simp[grepl(paste0("^", var_select[i], "[1-2]"), all_names_simp)])
-        new_var_select <- c(new_var_select, temp)
+        temp <- unique(all_names_simp[grepl(paste0("^", compartments[], "[1-2]"), all_names_simp)])
+        new_compartments <- c(new_compartments, temp)
       }
     }
-    var_select <- new_var_select
-  }
-  if(!all(var_select %in% all_names_simp)){
-    stop("Selected variable are not all present in output")
-  }
-
-  # Subsetting relevant columns
-  vars <- vars[,all_names_simp %in% var_select , ,drop = FALSE]
-
-  # Select components of data.frame
-  raw_names <- gsub("\\[.*?]", "", names(vars[1,,1]))
-  age_groups <- 1:table(raw_names)[1]
-  compartments <- unique(raw_names)
-  time <- vapply(seq_len(dim(x$output)[3]), function(y) {
-    rep(x$output[,"time",y],length(compartments) * length(age_groups))
-  }, FUN.VALUE = numeric(length(compartments) * length(age_groups) * nrow(x$output[,,1])))
-
-  # time <- as.vector(apply(x$output[,"time",, drop = FALSE], 2, function(x){
-  #   rep(x, length(compartments) * length(age_groups))
-  # }))
-
-  # Generating and filling output
-  out <- tidyr::expand_grid(replicate = 1:x$parameters$replicates,
-                            compartment = compartments,
-                            age_group = age_groups,
-                            t = x$output[,"time", 1])
-  out$t <- as.vector(time)
-  out$y <- as.vector(vars)
-
-  # If combine_compartments is TRUE, sum compartments of same type e.g.
-  # E1 and E2 together
-  if (combine_compartments == TRUE) {
-    out <- out %>%
-      dplyr::mutate(compartment = gsub("[1-2]$", "", .data$compartment)) %>%
-      dplyr::group_by(.data$replicate, .data$age_group, .data$compartment, .data$t) %>%
-      dplyr::summarise(y = sum(.data$y))
+    compartment_output_list <- lapply(new_compartments, function(j) {
+      odin_sv(x$output[,unlist(index[j]),], replicates = x$parameters$replicates, nt = nt)
+    })
+    names(compartment_output_list) <- new_compartments
+  } else {
+    compartment_output_list <- list()
   }
 
-  # Collapse ages
-  if(reduce_age){
-    out <- collapse_age(out)
+  # summaries
+  if (!is.null(var_select) & !is.null(summaries)) {
+    summaries_output_list <- vector(mode = "list", length = length(summaries))
+    for (i in 1:length(summaries)) {
+      indices <- which(summaries[i] %in% summary_variables)
+      temp_compartments <- summary_variable_compartments[[indices]]
+      summaries_output_list[[i]] <- odin_sv(x$output[,unlist(index[temp_compartments]),], replicates = x$parameters$replicates, nt = nt)
+    }
+    names(summaries_output_list) <- summaries
+  } else {
+    summaries_output_list <- list()
   }
 
-  # Add date
+  # combining outputs for compartments and overall summaries into 1 list
+  output_list <- c(compartment_output_list, summaries_output_list)
+  vars <- names(output_list)
+
+  # generating df of extracted compartment/summary outputs, disaggregated by age or not
+  if (reduce_age == TRUE) {
+    out <- data.frame("date" = as.numeric(x$output[,index$time,]),
+                      "replicate" = as.numeric(mapply(rep, seq_len(x$parameters$replicates), nt)),
+                      "compartment" = as.character(mapply(rep, vars, nt*x$parameters$replicates)),
+                      "y" = unlist(output_list))
+  } else {
+    out <- data.frame("date" = as.numeric(x$output[,index$time,]),
+                      "age_group" = bloop, ##### NEED TO CHANGE ####
+                      "replicate" = as.numeric(mapply(rep, seq_len(x$parameters$replicates), nt)),
+                      "compartment" = as.character(mapply(rep, vars, nt*x$parameters$replicates)),
+                      "y" = unlist(output_list))
+  }
+
+  # replacting time with date if date_0 is provided
   if(!is.null(date_0)){
     stopifnot(inherits(date_0, "Date"))
     out$date <- as.Date(out$t + date_0,
                         format = "%d/%m/%y")
   }
+
+  # If combine_compartments is TRUE, sum compartments of same type e.g.
+  # E1 and E2 together
+  if (combine_compartments == TRUE & reduce_age == FALSE) {
+    out <- out %>%
+      dplyr::mutate(compartment = gsub("[1-2]$", "", .data$compartment)) %>%
+      dplyr::group_by(.data$replicate, .data$age_group, .data$compartment, .data$date) %>%
+      dplyr::summarise(y = sum(.data$y))
+  } else if (combine_compartments == TRUE & reduce_age == TRUE) {
+    out <- out %>%
+      dplyr::mutate(compartment = gsub("[1-2]$", "", .data$compartment)) %>%
+      dplyr::group_by(.data$replicate, .data$compartment, .data$date) %>%
+      dplyr::summarise(y = sum(.data$y))
+  }
+
   return(out)
 }
 
